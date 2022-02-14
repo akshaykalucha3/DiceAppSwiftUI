3e802996907513020c148d0cade4225df3df4783
akshaykalucha3 <akshaykalucha@gmail.com>
2022-02-14 16:47:46 +0530

    initital

---
+++ diceapp/diceappApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceappApp.swift
+//  diceapp
+//
+//  Created by Akshay Kalucha on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct diceappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
